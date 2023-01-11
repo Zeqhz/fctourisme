5191f0b0f1d2088edc413c6dbca6c1a27b327011
Clovis Ede <clovis.ede@orange.fr>
2023-01-11 10:32:45 +0100

    connexion et deco

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,4 +21,8 @@ class SecurityController extends AbstractController
 
         ]);
     }
+    #[Route('/logout', name: 'app_logout' ,methods: ['GET'])]
+    public function logout() : Response {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
